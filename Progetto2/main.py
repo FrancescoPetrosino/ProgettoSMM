f003e7e7adf67d722f14c01c2cec830179c196e2
FrancescoPetrosino <francesco-petrosino@outlook.it>
2023-01-23 19:28:06 +0100

    commit

--- Progetto2/main.py
+++ Progetto2/main.py
@@ -1,7 +1,5 @@
 import json
 import pandas
-import retry
-import spotipy as sp
 
 def export_to_json(file_name, data):
 	with open(file_name, 'w') as f:
@@ -12,7 +10,3 @@ def export_to_csv(file_name, dataframe):
 
 def import_from_csv(file_name) -> pandas.DataFrame:
 	return pandas.read_csv(file_name, sep=";")
-
-@retry(tries=5, delay=5)
-def playlist_items(playlist_id):
-    return sp.playlist_items(playlist_id=playlist_id, limit=100)
